256c28fa5af54d531d0b8f76e56b511c3ed5302f
Shray Kumar Singh <96106431+shray732002@users.noreply.github.com>
2022-07-07 12:42:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 from flask import Flask,request,render_template
+rom nltk.tokenize.punkt import PunktSentenceTokenizer
 from sklearn.svm import SVC
 ps = PorterStemmer()
 
